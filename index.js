3d611b5feb764feaf4b34481de48e224147a4cb8
Yahya <yahyahussein670@gmail.com>
2022-06-03 16:25:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+var deleteMain = document.getElementById('main').remove()
+
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Yahya is the champion';
